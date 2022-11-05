639a0cff94e6509ce3b172ad865ec5486f221d04
Cawiess <carl.wictor.essman@gmail.com>
2022-11-05 14:00:23 +0100

    adding placeholder content, styles and statics

--- django_project/settings.py
+++ django_project/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static" # path to global static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
