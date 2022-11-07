cb4d93c21de8d37425f703d9a273ce48a52eee5f
Cawiess <carl.wictor.essman@gmail.com>
2022-11-07 11:15:22 +0100

    interpolating dummy data into latest posts

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,14 @@ from django.shortcuts import render
 
 # Some temporary dummy data for posts
 
-posts = [
+all_posts = [
     {
-        "slug": "hike-in-the-mountains",
+        "slug": "placeholders-are-great",
         "image": "mountains.jpg",
         "author": "Carl",
         "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "title": "Post dummy title #1",
+        "excerpt": "There's nothing like the placeholders you get when doing Django! I truly enjoy it!",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
@@ -27,12 +27,12 @@ posts = [
         """
     },
     {
-        "slug": "programming-is-fun",
+        "slug": "placeholders-are-fun",
         "image": "coding.jpg",
         "author": "Carl",
         "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "title": "Post dummy title #2",
+        "excerpt": "Did you ever spend a whole night thinking about placeholders? I just love it!",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
@@ -48,12 +48,12 @@ posts = [
         """
     },
     {
-        "slug": "into-the-woods",
+        "slug": "placeholders-are-healthy",
         "image": "woods.jpg",
         "author": "Carl",
         "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "title": "Post dummy title #3",
+        "excerpt": "Placeholders is amazing! The amount of inspiration I feel from writing them is immense!",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
@@ -70,10 +70,17 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post.get('date')
+
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
     return render(request, "blog/all-posts.html")
