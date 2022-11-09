2d0534ff040d50f29035320808e5b6bab704e1d4
Cawiess <carl.wictor.essman@gmail.com>
2022-11-09 15:44:18 +0100

    using data from database to populate pages

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,17 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
+from .models import Post
 
 # Some temporary dummy data for posts
 
+'''
 all_posts = [
     {
         "slug": "placeholders-are-great",
         "image": "mountains.jpg",
         "author": "Carl",
         "date": date(2021, 7, 21),
-        "title": "Post dummy title #1",
+        "title": "Post dummy title one",
         "excerpt": "There's nothing like the placeholders you get when doing Django! I truly enjoy it!",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
@@ -31,7 +32,7 @@ all_posts = [
         "image": "coding.jpg",
         "author": "Carl",
         "date": date(2022, 3, 10),
-        "title": "Post dummy title #2",
+        "title": "Post dummy title two",
         "excerpt": "Did you ever spend a whole night thinking about placeholders? I just love it!",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
@@ -52,7 +53,7 @@ all_posts = [
         "image": "woods.jpg",
         "author": "Carl",
         "date": date(2020, 8, 5),
-        "title": "Post dummy title #3",
+        "title": "Post dummy title three",
         "excerpt": "Placeholders is amazing! The amount of inspiration I feel from writing them is immense!",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
@@ -70,25 +71,28 @@ all_posts = [
     }
 ]
 
-def get_date(post):
-    return post.get('date')
+'''
 
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    #sorted_posts = sorted(all_posts, key=get_date)
+    #latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug']==slug)
+    #identified_post = next(post for post in all_posts if post['slug']==slug)
+    identified_post = get_object_or_404(Post, slug=slug) #Post.objects.get(slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
