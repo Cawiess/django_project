13a0e806830710e4893ca794d9682901a3f1cd7d
Cawiess <carl.wictor.essman@gmail.com>
2022-11-09 10:15:21 +0100

    fixing typos, creating database and run migrations

--- blog/models.py
+++ blog/models.py
@@ -7,17 +7,17 @@ class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
 class Author(models.Model):
-    first_name = models.CHarfield(max_length=100)
-    last_name = models.CHarfield(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    excerpt = models.Charfield(max_length=200)
+    excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
 
