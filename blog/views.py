b4bf5c90b2cf8798fa11f1ce59bb5dd7986591ba
Cawiess <carl.wictor.essman@gmail.com>
2022-11-20 15:19:50 +0100

    converting functional views to class based

--- blog/views.py
+++ blog/views.py
@@ -1,98 +1,47 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 # Some temporary dummy data for posts
 
-'''
-all_posts = [
-    {
-        "slug": "placeholders-are-great",
-        "image": "mountains.jpg",
-        "author": "Carl",
-        "date": date(2021, 7, 21),
-        "title": "Post dummy title one",
-        "excerpt": "There's nothing like the placeholders you get when doing Django! I truly enjoy it!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "placeholders-are-fun",
-        "image": "coding.jpg",
-        "author": "Carl",
-        "date": date(2022, 3, 10),
-        "title": "Post dummy title two",
-        "excerpt": "Did you ever spend a whole night thinking about placeholders? I just love it!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "placeholders-are-healthy",
-        "image": "woods.jpg",
-        "author": "Carl",
-        "date": date(2020, 8, 5),
-        "title": "Post dummy title three",
-        "excerpt": "Placeholders is amazing! The amount of inspiration I feel from writing them is immense!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-'''
-
 # Create your views here.
 
-def starting_page(request):
-    #sorted_posts = sorted(all_posts, key=get_date)
-    #latest_posts = sorted_posts[-3:]
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
 
+        return data
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordring = ["-date"]
+    context_object_name = "all_posts"
+
+
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
+
+        return context
+
+
+'''
 def post_detail(request, slug):
-    #identified_post = next(post for post in all_posts if post['slug']==slug)
     identified_post = get_object_or_404(Post, slug=slug) #Post.objects.get(slug=slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post,
         "post_tags": identified_post.tags.all()
     })
+'''
